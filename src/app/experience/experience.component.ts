87bf76466fbc7c717c7823fcb55e04ea91272ca5
raquelbcarballo <raquelbc2996@gmail.com>
2023-11-09 20:42:13 +0100

    update css

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -10,37 +10,6 @@ export class ExperienceComponent implements OnInit {
 
   constructor() { }
 
-  experienceModules: ExperienceModule[] = [
-    new ExperienceModule("💻 Full-Stack Engineer",
-      "August 2022 - Present",
-      "Efimob",
-      "Company in charge of the electric mobility sector.",
-      ["Go", "Angular","HTML", "CSS", "TypeScript", "NestJS","MQTT", "AWS","Git"]),
-    new ExperienceModule("💻 Full-Stack Engineer",
-      "June 2021 - August 2022",
-      "Salcotech S.L.",
-      "Company in charge of the development of a new technology in relation to the tire.",
-      ["Flutter", "Vue", "HTML", "CSS", "JavaScript", "Python", "Flask", "MySQL", "AWS", "FreeCad","Git"]),
-    new ExperienceModule("🩰 Dance teacher",
-      "September 2020 - Present",
-      "Inés Núñez",
-      "Classes for children between 3 and 12 years old", []),
-    new ExperienceModule("💻 Developer Engineer",
-      "September 2019 - June 2021",
-      "i+D3",
-      "Company in charge of the development and instalation of parkings.",
-      ["C++", "QA","Git"]),
-    new ExperienceModule("🩰 Dance teacher",
-      "October 2019 - April 2020",
-      "Baas Ballet",
-      "Classes for children between 6 and 16 years old", []),
-    new ExperienceModule("🤖 Industrial Engineer",
-      "July 2017 - August 2017",
-      "Ledisson AIT",
-      "Company in charge of the automatization of process to increase productivity, eficience and solve problems.",
-      ["i+D", "ABB", "Kuka", "Fanuc"]),
-  ];
-  
   ngOnInit(): void {
   }
 
--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -7,37 +7,37 @@ import { ExperienceModule } from '../experience/experience.module';
   templateUrl: './experiences.component.html',
   styleUrls: ['./experiences.component.css']
 })
-export class ExperiencesComponent implements OnInit {  
-  experiences: ExperienceComponent[]=[]
+export class ExperiencesComponent implements OnInit {
+  experiences: ExperienceComponent[] = []
   experienceModules: ExperienceModule[] = [
-    new ExperienceModule("💻 Full-Stack Engineer",
+    new ExperienceModule("Full-Stack Engineer",
       "August 2022 - Present",
       "Efimob",
       "Company in charge of the electric mobility sector.",
-      ["Go", "Angular", "NestJS", "AWS"]),
-    new ExperienceModule("💻 Full-Stack Engineer",
+      ["Go", "Angular", "NestJS", "AWS", "Python", "Machine-Learning"]),
+    new ExperienceModule("Full-Stack Engineer",
       "June 2021 - August 2022",
       "Salcotech S.L.",
       "Company in charge of the development of a new technology in relation to the tire.",
       ["Flutter", "Vue", "Flask", "MySQL", "AWS", "FreeCad"]),
-    new ExperienceModule("🩰 Dance teacher",
-      "September 2020 - Present",
-      "Inés Núñez",
-      "Classes for children between 3 and 12 years old", []),
-    new ExperienceModule("💻 Developer Engineer",
+    new ExperienceModule("Developer Engineer",
       "September 2019 - June 2021",
       "i+D3",
       "Company in charge of the development and instalation of parkings.",
       ["C++", "QA"]),
-    new ExperienceModule("🩰 Dance teacher",
-      "October 2019 - April 2020",
-      "Baas Ballet",
-      "Classes for children between 6 and 16 years old", []),
-    new ExperienceModule("🤖 Industrial Engineer",
+    new ExperienceModule("Industrial Engineer",
       "July 2017 - August 2017",
       "Ledisson AIT",
       "Company in charge of the automatization of process to increase productivity, eficience and solve problems.",
       ["i+D", "ABB", "Kuka", "Fanuc"]),
+    new ExperienceModule("Dance teacher",
+      "September 2020 - March 2023",
+      "Inés Núñez",
+      "Classes for children between 3 and 12 years old", []),
+    new ExperienceModule("Dance teacher",
+      "October 2019 - April 2020",
+      "Baas Ballet",
+      "Classes for children between 6 and 16 years old", []),
   ];
   constructor() { }
 
